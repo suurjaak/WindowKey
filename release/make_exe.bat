45d6f7f762fccdb17bd8f58e360a285be00af4ca
Erki Suurjaak <erki@lap.ee>
2014-01-15 22:25:27 +0200

    Updated compile and install scripts.

--- release/make_exe.bat
+++ release/make_exe.bat
@@ -1,1 +1,24 @@
-start Ahk2Exe.exe /in ../WindowKey.ahk /out WindowKey.exe /icon ../Icon.ico
+:: Creates an executable from AutoHotkey script and compresses it with Mpress.
+::
+:: @author    Erki Suurjaak
+:: @created   08.01.2014
+:: @modified  15.01.2014
+@echo off
+set NAME=WindowKey
+set SOURCEDIR=..
+set SCRIPTFILE=%SOURCEDIR%\%NAME%.ahk
+set EXEFILE=%NAME%.exe
+set ICONFILE=%SOURCEDIR%\Icon.ico
+
+if exist %EXEFILE% (echo Removing previous %EXEFILE%. & del "%EXEFILE%")
+echo Compiling %EXEFILE%.
+start Ahk2Exe.exe /in "%SCRIPTFILE%" /out "%EXEFILE%" /icon "%ICONFILE%"
+timeout /t 1 > NUL
+
+if not exist "%EXEFILE%" (echo ERROR: %EXEFILE% not found. & goto :EOF)
+for %%F in ("%EXEFILE%") do echo %EXEFILE% uncompressed size: %%~zF.
+mpress "%EXEFILE%"
+if errorlevel 1 goto :EOF
+
+echo.
+for %%F in ("%EXEFILE%") do echo %EXEFILE% compressed size: %%~zF.
--- release/make_setup.bat
+++ release/make_setup.bat
@@ -1,14 +1,28 @@
+:: Creates a setup distribution of AutoHotkey executable using NullSoft
+:: Scriptable Install System.
+::
+:: @author    Erki Suurjaak
+:: @created   08.01.2014
+:: @modified  15.01.2014
 @echo off
-set VERSION=%1%
-if "%1" == "" set VERSION=1.0
+set NAME=WindowKey
+set EXEFILE=%NAME%.exe
+set SOURCEDIR=..
+set SCRIPTFILE=%SOURCEDIR%\%NAME%.ahk
+if not exist "%EXEFILE%" if exist "make_exe.bat" (
+	echo %EXEFILE% missing, calling make_exe.
+	call make_exe.bat
+)
+if not exist "%EXEFILE%" (echo %EXEFILE% not detected, exiting. & goto :EOF)
 
-pushd ..
-set SOURCEDIR=%CD%
-popd
+set VERSIONLINE=""
+for /f "tokens=2 delims==""" %%G in ('find "VERSION " %SCRIPTFILE% ^| findstr VERSION') do set VERSIONLINE=%%G
+if ERRORLEVEL 0 set VERSION=%VERSIONLINE:~2,-1%
+if "%VERSION%" == "" (echo Version not detected, exiting. & goto :EOF)
 
-set DESTFILE=windowkey_%VERSION%_setup.exe
-echo Creating installer for WindowKey %VERSION%.
-if exist "%DESTFILE%" del "%DESTFILE%"
+set DESTFILE=%NAME%_%VERSION%_setup.exe
+echo Creating installer for %NAME% %VERSION%.
+if exist "%DESTFILE%" (echo Removing previous %DESTFILE%. & del "%DESTFILE%")
 set NSISDIR=C:\Program Files (x86)\Nullsoft Scriptable Install System
 if not exist "%NSISDIR%" set NSISDIR=C:\Program Files\Nullsoft Scriptable Install System
 if not exist "%NSISDIR%" set NSISDIR=C:\Program Files (x86)\NSIS
@@ -17,4 +31,5 @@ if not exist "%NSISDIR%" set NSISDIR=C:\Program Files\NSIS
 if ERRORLEVEL 1 goto :EOF
 
 echo.
-echo Successfully created WindowKey source distribution %DESTFILE%.
+for /f %%F in ('dir /b %DESTFILE%') do set DESTFILE=%%F
+if exist "%DESTFILE%" echo Successfully created %NAME% v%VERSION% source distribution %DESTFILE%.
